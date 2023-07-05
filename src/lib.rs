3185583e3a4dfa79e2c7e8719a5b2b07c5a9c140
KunoiSayami <i@leanhe.dev>
2023-07-05 14:25:31 +0800

    feat: Add database functions
    Signed-off-by: KunoiSayami <i@leanhe.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,11 @@ pub mod time {
     pub fn get_current_timestamp() -> u128 {
         get_current_duration().as_millis()
     }
+
+    pub fn get_current_second() -> u64 {
+        get_current_duration().as_secs()
+    }
+
     pub fn get_current_duration() -> Duration {
 
         let start = std::time::SystemTime::now();
@@ -16,6 +21,38 @@ pub mod time {
             .expect("Time went backwards")
 
     }
+}
+
+#[cfg(feature = "sqlx")]
+pub mod sqlx {
+    use sqlx::SqliteConnection;
+
+    async fn update_database_version(conn: &mut SqliteConnection, table_name: &'static str, version: &'static str) -> Result<(), sqlx::Error> {
+        sqlx::query(&format!(r#"UPDATE "{}" SET "value" = ? WHERE "key" = 'version'"#, table_name))
+            .bind(version)
+            .execute(conn)
+            .await
+            .map(|_| ())
+    }
+
+    async fn insert_database_version(conn: &mut SqliteConnection, table_name: &'static str, version: &'static str) -> Result<(), sqlx::Error> {
+        sqlx::query(&format!(r#"INSERT INTO "{}" VALUES ("version", ?)"#, table_name))
+            .bind(version)
+            .execute(conn)
+            .await
+            .map(|_| ())
+    }
+
+    async fn check_database(conn: &mut SqliteConnection, table_name: &'static str) -> Result<bool, sqlx::Error> {
+        sqlx::query_as(
+            r#"SELECT COUNT(*) FROM "sqlite_master" WHERE "type" = 'table' AND "name" = 'meta';"#,
+        )
+            .bind(table_name)
+            .fetch_one(conn)
+            .await
+            .map(|(count,): (i32,)| count > 0)
+    }
+
 }
 
 #[cfg(test)]
